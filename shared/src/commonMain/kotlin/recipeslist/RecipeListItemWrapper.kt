98f2d2d563f52c0d7d8f00e846a061595edd535e
Abdul Basit <abdulbasit5361234@gmail.com>
2023-07-19 22:26:32 +0500

    animate via Animatable

--- shared/src/commonMain/kotlin/recipeslist/RecipeListItemWrapper.kt
+++ shared/src/commonMain/kotlin/recipeslist/RecipeListItemWrapper.kt
@@ -4,75 +4,57 @@ package recipeslist
  * Created by abdulbasit on 18/06/2023.
  */
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 
-const val perspectiveValue = 0.004
 
 @Composable
 fun RecipeListItemWrapper(
-    child: @Composable () -> Unit,
-    scrollDirection: ScrollDirection = ScrollDirection.Backward
+    child: @Composable () -> Unit, scrollDirection: Boolean
 ) {
-    var visible by remember { mutableStateOf(false) }
-    val camera by animateFloatAsState(
-        targetValue = if (visible) 8.0f else 7.0f,
-        animationSpec = tween(
-            durationMillis = 500,
-            easing = CubicBezierEasing(0f, 0.5f, 0.5f, 1f)
-        )
-    )
-    val scale by animateFloatAsState(
-        targetValue = if (visible) 1f else 0.7f,
-        animationSpec = tween(
-            durationMillis = 700,
-            easing = CubicBezierEasing(0f, 0.5f, 0.5f, 1f)
-        )
-    )
-    val perspective by animateFloatAsState(
-        targetValue = if (visible) 0f else {
-            when (scrollDirection) {
-                ScrollDirection.Forward -> 60f
-                ScrollDirection.Backward -> -60f
-            }
-        },
-        animationSpec = spring(
-            dampingRatio = 0.6f,
-            stiffness = 500f
-        )
-    )
+    val cameraAnimatable = remember { Animatable(initialValue = 7.0f) }
+    val scaleAnimatable = remember { Animatable(initialValue = 0.7f) }
 
     LaunchedEffect(Unit) {
-        visible = true
+        cameraAnimatable.animateTo(
+            8.0f, animationSpec = tween(
+                durationMillis = 500, easing = CubicBezierEasing(0f, 0.5f, 0.5f, 1f)
+            )
+        )
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .graphicsLayer {
-                scaleX = scale
-                scaleY = scale
-                rotationX = perspective
-                cameraDistance = camera
-            }
-    ) {
+    LaunchedEffect(Unit) {
+        scaleAnimatable.animateTo(
+            1f, animationSpec = tween(
+                durationMillis = 700, easing = CubicBezierEasing(0f, 0.5f, 0.5f, 1f)
+            )
+        )
+    }
+
+    val perspectiveAnimatable =
+        remember { Animatable(initialValue = if (scrollDirection) 60f else -60f) }
+    LaunchedEffect(Unit) {
+        perspectiveAnimatable.animateTo(
+            0f, animationSpec = tween(
+                durationMillis = 700, easing = CubicBezierEasing(0f, 0.5f, 0.5f, 1f)
+            )
+        )
+    }
+
+    Box(modifier = Modifier.fillMaxSize().graphicsLayer {
+        scaleX = scaleAnimatable.value
+        scaleY = scaleAnimatable.value
+        rotationX = perspectiveAnimatable.value
+        cameraDistance = cameraAnimatable.value
+    }) {
         child()
     }
-}
-
-enum class ScrollDirection {
-    Forward,
-    Backward
 }
