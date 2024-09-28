c054a78c448e85f3897fd8d1a3d620b70667dfef
Abdul Basit <abdulbasit5361234@gmail.com>
2024-09-28 23:20:34 +0300

    Remove SensorManager dependency from MainView
    This changes the MainView function to no longer initialize or use the SensorManager instance directly. Instead, it passes null to the App function, implying that sensor functionality is no longer required for desktop.

--- shared/src/desktopMain/kotlin/main.desktop.kt
+++ shared/src/desktopMain/kotlin/main.desktop.kt
@@ -1,9 +1,7 @@
 import androidx.compose.runtime.Composable
-import sensor.SensorManager
 
 
 @Composable
 fun MainView() {
-    val sensorManager = SensorManager()
-    App(sensorManager, true)
+    App(null, true)
 }
